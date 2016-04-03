fa99689049087872571b76a88130c521248e50d2
petrospe <petros_pe@yahoo.gr>
2016-04-04 00:10:48 +0300

    modified:   public/js/calendarController.js

--- public/js/calendarController.js
+++ public/js/calendarController.js
@@ -4,7 +4,7 @@ var pathArray = window.location.pathname.split( '/' );
 var calendarurl = pathArray[0]+'/restful11/api/index.php/tasks';
 
 $(document).ready(function() {
-    $('#taskinsert').append(
+    $('#taskpopup').append(
         $('<div/>').append(
             $('<input/>', {
                 type: 'text',
@@ -27,81 +27,66 @@ $(document).ready(function() {
                 placeholder: 'End datetime'
             }),
             $('<button/>', {
-                        type: 'submit',
-                        text: 'Submit'
+                type: 'submit',
+                text: 'Submit'
             }).addClass('btn btn-primary')
         )
     )
 });
 
 $(document).ready(function() {
-
-        $('#calendar').fullCalendar({
-                header: {
-                        left: 'prev,next today',
-                        center: 'title',
-                        right: 'month,agendaWeek,agendaDay'
-                },
-                defaultDate: Date(),
-                selectable: true,
-                selectHelper: true,
-//                select: function(start, end){
-//                    $('#taskinsert').show(); // show's pop up
-//                    $('input').focus(); // auto focus on the field
-//                    var start = Date.parse(start) / 1000; // parse the start time to retain value
-//                    var end = Date.parse(end) / 1000; // same but with end
-//                    $('.btn btn-primary').click(function(){ // on submission
-//                        var title = $('#title').val(); // gets title value
-//                        if(title){ // if the title exists run script
-//                            $.post(pathArray[0]+'/restful11/api/index.php/task', {title: title, start:start, end: end, allDay: allDay}, // be sure to filter data 
-//                            function(){
-//                            $('#title').val(''); // clear title field
-//                            start = ''; // clear start time
-//                            end = ''; // clear end time
-//                            calendar.fullCalendar('unselect');
-//                            calendar.fullCalendar('refetchEvents');
-//                            });
-//                        }
-//                    $('#taskinsert').hide(); // hide pop up box
-//                    });
-//                },
-                select: function(start, end) {
-                        var title = prompt('Event Title:');
-//                        var description = prompt('Enter Description:');
-                        var eventData;
-                        if (title) {
-                                eventData = {
-                                        title: title,
-//                                        description: description,
-//                                        url: 'javascript:fee_add("' + title + '","' + start +'");',,
-//                                        start: new Date(),
-//                                        end: new Date()
-                                };
-                                $.ajax({
-                                    url: pathArray[0]+'/restful11/api/index.php/task',
-                                    type: 'POST',
-                                    async: false,
-                                    data: eventData,
-                                    datatype: 'json',
-                                    success:function(msg){
-                                        if(msg){
-                                            alert('Task was added');
-                                            location.reload();
-                                        }else{
-                                            alert('Task cannot added');
-                                        }
-                                    }
-                                }),
-//                                $.post(pathArray[0]+'/restful11/api/index.php/task'),
-                                $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        defaultDate: new Date(),
+        selectable: true,
+        selectHelper: true,
+        select: function(start, end) {
+            var title = prompt('Event Title:');
+            var start=moment(start).format('YYYY-MM-DD HH:mm:ss');
+            var end=moment(end).format('YYYY-MM-DD HH:mm:ss');
+            var eventData;
+            if (title) {
+                eventData = {
+                    title: title,
+//                  url: 'javascript:fee_add("' + title + '","' + start +'");',,
+                    start: start,
+                    end: end
+//                  allDay : false
+                };
+                $.ajax({
+                    url: pathArray[0]+'/restful11/api/index.php/task',
+                    type: 'POST',
+                    async: false,
+                    data: eventData,
+                    datatype: 'json',
+                    success:function(msg){
+                        if(msg){
+                            alert('Task was added');
+//                          location.reload();
+                        }else{
+                            alert('Task cannot added');
                         }
-                        
-                        $('#calendar').fullCalendar('unselect');
-                },
-                
-                editable: true,
-                eventLimit: true, // allow "more" link when too many events
-                events: calendarurl
-                
-        });
+                    }
+                }),
+//              $.post(pathArray[0]+'/restful11/api/index.php/task'),
+                $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+            }
+            $('#calendar').fullCalendar('unselect');
+        },
+        eventClick: function(calEvent, jsEvent, view) {
+            alert('Event: ' + calEvent.title + ' ,DateTime: '+ calEvent.start);
+//          alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+//          alert('View: ' + view.name);
+
+            // change the border color just for fun
+            $(this).css('border-color', 'red');
+        },
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: calendarurl
+    });
 });
