c3a0070f0f481effe95926135e8e4c21b2f8bb9f
petrospe <petros_pe@yahoo.gr>
2016-08-12 18:00:05 +0300

    new file:   api/.htaccess 	new file:   public/images/favicon.png 	modified:   public/templates/frontpage.twig

--- public/templates/cache/cf/cfc0c4fad1c08fb6ab94bfb3d0293acc4927fa9db1c6de33066fd98df9bd86e5.php
+++ public/templates/cache/cf/cfc0c4fad1c08fb6ab94bfb3d0293acc4927fa9db1c6de33066fd98df9bd86e5.php
@@ -28,14 +28,15 @@ class __TwigTemplate_140c9b1cdabde70e9baa16fc0a547b38681f0d5e465752b3666570c3dc0
         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
         <meta name=\"description\" content=\"Manage tasks web application\">
         <meta name=\"author\" content=\"Peter Petropoulos\">
+\t\t<link rel=\"icon\" href=\"images/favicon.png\" type=\"image/png\" sizes=\"16x16\">
         <link rel=\"stylesheet\" type=\"text/css\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\">
         <link rel=\"stylesheet\" type=\"text/css\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css\">
         <link href=\"//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css\" rel=\"stylesheet\"/>
         <link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\" />
         ";
-        // line 14
-        $this->displayBlock('stylesheets', $context, $blocks);
         // line 15
+        $this->displayBlock('stylesheets', $context, $blocks);
+        // line 16
         echo "    </head>
     <body>
         <div class=\"container\">
@@ -54,45 +55,45 @@ class __TwigTemplate_140c9b1cdabde70e9baa16fc0a547b38681f0d5e465752b3666570c3dc0
                         <ul class=\"nav navbar-nav\">
                             <li>
                                 <a href=\"tasks\" class=\"btn ";
-        // line 32
+        // line 33
         echo twig_escape_filter($this->env, (isset($context["memberClass"]) ? $context["memberClass"] : null), "html", null, true);
         echo "\"><span class=\"glyphicon glyphicon-calendar\"></span> Tasks</a>
                             </li>
                             <li>
                                 <a href=\"users\" class=\"btn ";
-        // line 35
+        // line 36
         echo twig_escape_filter($this->env, (isset($context["adminClass"]) ? $context["adminClass"] : null), "html", null, true);
         echo "\"><span class=\"glyphicon glyphicon-th-list\"></span> Users</a>
                             </li>
                         </ul>
                         <ul class=\"nav navbar-nav navbar-right\">
                             ";
-        // line 39
+        // line 40
         if ((isset($context["hasIdentity"]) ? $context["hasIdentity"] : null)) {
-            // line 40
+            // line 41
             echo "                                <li class=\"identity\"><span class=\"glyphicon glyphicon-user\"></span> ";
             echo twig_escape_filter($this->env, twig_capitalize_string_filter($this->env, $this->getAttribute((isset($context["identity"]) ? $context["identity"] : null), "username", array(), "array")), "html", null, true);
             echo "</li>
                                 <li><a href=\"logout\" class=\"btn\"><span class=\"glyphicon glyphicon-log-out\"></span> Logout</a></li>
                             ";
         } else {
-            // line 43
+            // line 44
             echo "                                <li><a href=\"login\" class=\"btn\"><span class=\"glyphicon glyphicon-log-in\"></span> Login</a></li>
                             ";
         }
-        // line 45
+        // line 46
         echo "                        </ul>
                     </div>
                 </div>
             </nav>
             ";
-        // line 49
+        // line 50
         echo (isset($context["intro"]) ? $context["intro"] : null);
         echo "
             ";
-        // line 50
-        $this->displayBlock('content', $context, $blocks);
         // line 51
+        $this->displayBlock('content', $context, $blocks);
+        // line 52
         echo "            <div>
             <hr>
                 <footer>
@@ -124,25 +125,25 @@ class __TwigTemplate_140c9b1cdabde70e9baa16fc0a547b38681f0d5e465752b3666570c3dc0
         <script src=\"//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/js/bootstrap-editable.min.js\"></script>
         <script type=\"text/javascript\" src=\"js/scripts.js\"></script>
         ";
-        // line 81
-        $this->displayBlock('javascript', $context, $blocks);
         // line 82
+        $this->displayBlock('javascript', $context, $blocks);
+        // line 83
         echo "    </body>
 </html>";
     }
 
-    // line 14
+    // line 15
     public function block_stylesheets($context, array $blocks = array())
     {
     }
 
-    // line 50
+    // line 51
     public function block_content($context, array $blocks = array())
     {
         echo " ";
     }
 
-    // line 81
+    // line 82
     public function block_javascript($context, array $blocks = array())
     {
         echo " ";
@@ -160,7 +161,7 @@ class __TwigTemplate_140c9b1cdabde70e9baa16fc0a547b38681f0d5e465752b3666570c3dc0
 
     public function getDebugInfo()
     {
-        return array (  146 => 81,  140 => 50,  135 => 14,  130 => 82,  128 => 81,  96 => 51,  94 => 50,  90 => 49,  84 => 45,  80 => 43,  73 => 40,  71 => 39,  64 => 35,  58 => 32,  39 => 15,  37 => 14,  22 => 1,);
+        return array (  147 => 82,  141 => 51,  136 => 15,  131 => 83,  129 => 82,  97 => 52,  95 => 51,  91 => 50,  85 => 46,  81 => 44,  74 => 41,  72 => 40,  65 => 36,  59 => 33,  40 => 16,  38 => 15,  22 => 1,);
     }
 }
 /* <!-- Frontpage Template file -->*/
@@ -172,6 +173,7 @@ class __TwigTemplate_140c9b1cdabde70e9baa16fc0a547b38681f0d5e465752b3666570c3dc0
 /*         <meta name="viewport" content="width=device-width, initial-scale=1">*/
 /*         <meta name="description" content="Manage tasks web application">*/
 /*         <meta name="author" content="Peter Petropoulos">*/
+/* 		<link rel="icon" href="images/favicon.png" type="image/png" sizes="16x16">*/
 /*         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">*/
 /*         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">*/
 /*         <link href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet"/>*/
