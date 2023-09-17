79c3aaa8c77b39f8e744b5fa6f55cbace853dd08
petrospe <petrozpe@gmail.com>
2023-09-18 00:43:43 +0300

    fix error reporting

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ date_default_timezone_set('Europe/Athens');
 // error_reporting(-1);
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
+error_reporting(0);
+ini_set('display_errors', '0');
 
 require '../vendor/autoload.php';
 \Slim\Slim::registerAutoloader();
